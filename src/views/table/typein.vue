f747cbbb4fbd2a458c6dd3931c4bb23d830793d3
戴峰 <daifeng@cmiot.chinamobile.com>
2020-11-30 18:19:19 +0800

    update

--- src/views/table/typein.vue
+++ src/views/table/typein.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="app-container">
-    <el-select v-model="value" filterable allow-create default-first-option placeholder="指标选择" size="medium">
-      <el-option v-for="item in options" :key="item" :label="item" :value="item" />
-    </el-select>
+    <el-row>
+      <el-col :span="3">
+        <el-select v-model="carIdxSelected" filterable allow-create default-first-option placeholder="指标选择" size="medium">
+          <el-option v-for="item in options" :key="item" :label="item" :value="item" />
+        </el-select>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="primary" size="small" @click="onAddIdxBtnClick()">新增指标</el-button>
+      </el-col>
+    </el-row>
     <el-divider />
     <el-table v-loading="listLoading" :data="list" element-loading-text="Loading" border fit highlight-current-row>
       <el-table-column align="center" label="编号" width="95">
@@ -21,6 +28,15 @@
         </template>
       </el-table-column>
     </el-table>
+    <el-dialog title="指标新增" :visible.sync="addIdxDlgVisible" width="30%">
+      <el-input v-model="addIdxDlgData.carIdxName" placeholder="指标名称" class="row-idx" />
+      <el-input v-model="addIdxDlgData.carServiceName" placeholder="竞品名称" class="row-idx" />
+      <el-input v-model="addIdxDlgData.carIdxValue" placeholder="车型值" class="row-idx" />
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" size="mini" @click="handleAddIdxDlgClose()">新增并继续</el-button>
+        <el-button size="mini" @click="addIdxDlgVisible = false">取 消</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -40,16 +56,34 @@ export default {
   },
   data() {
     return {
+      addIdxDlgVisible: false,
       list: null,
       listLoading: false,
       options: [],
-      value: []
+      carIdxSelected: '',
+      addIdxDlgData: {
+        carIdxName: '',
+        carServiceName: '',
+        carIdxValue: ''
+      }
     }
   },
   created() {
     this.fetchCarIdxNames()
   },
   methods: {
+    // 指标添加按钮点击
+    onAddIdxBtnClick() {
+      this.addIdxDlgVisible = true
+      this.addIdxDlgData.carIdxName = this.carIdxSelected
+    },
+
+    // 指标添加窗口关闭之前
+    handleAddIdxDlgClose() {
+      this.addIdxDlgVisible = false
+      console.log('指标窗口关闭')
+    },
+
     fetchData() {
       this.listLoading = true
       getList().then(response => {
@@ -57,10 +91,15 @@ export default {
         this.listLoading = false
       })
     },
+
+    // 查询指标名称
     fetchCarIdxNames() {
       getUniqueIdxNames().then(response => {
         if (response.data) {
           this.options = response.data
+          if (this.options.length > 0) {
+            this.carIdxSelected = this.options[0]
+          }
         } else {
           this.options = []
         }
@@ -69,3 +108,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.row-idx {
+  margin-block: 6px;
+}
+</style>
